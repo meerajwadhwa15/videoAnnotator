85d49d81b21e0b121c8f66022fe5a12b85406409
davenguyenhuy <nguyenhuycuong.uet@gmail.com>
2021-07-01 15:31:09 +0700

    fix errors when deploy to vercel - next-i18next error

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,10 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const path = require('path');
+
 module.exports = {
   i18n: {
     locales: ['en', 'vi'],
     defaultLocale: 'en',
+    localePath: path.resolve('./public/locales'),
   },
 };
