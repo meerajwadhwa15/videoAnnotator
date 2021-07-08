3bc30d7381fb7c5816d9d04ad7577e5b18c22515
davenguyenhuy <nguyenhuycuong.uet@gmail.com>
2021-07-08 13:31:14 +0700

    protect pages with authenticated hoc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
 import Home from 'modules/home';
+import { withAuthPage } from 'utils/hoc';
 
 function Index() {
   return (
@@ -14,12 +15,12 @@ function Index() {
   );
 }
 
-export const getServerSideProps = async ({ locale }) => {
+export const getServerSideProps = withAuthPage(async ({ locale }) => {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common', 'about'])),
+      ...(await serverSideTranslations(locale || '', ['common', 'about'])),
     },
   };
-};
+});
 
 export default Index;
