bf34a63472b8e13b26eadf3cbaeb781ed85b47f2
phuongdk <phuong.do@hcl.com>
2021-07-12 11:47:00 +0700

    fix some bugs

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -1,12 +1,27 @@
 import { Container, Row, Col } from 'shards-react';
 
 const AuthLayout = ({ children }) => (
-  <Container fluid className="h-100 px-4">
+  <Container fluid className="h-100 pl-0 pr-0">
     <Row noGutters className="h-100">
       <Col lg="3" md="5" className="auth-form mx-auto my-auto">
         {children}
       </Col>
     </Row>
+    <footer
+      style={{
+        position: 'fixed',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        bottom: '0',
+        width: '100%',
+        height: '50px',
+        backgroundColor: '#ffffff',
+        borderTop: '1px solid #e0e0e0',
+      }}
+    >
+      <div style={{ color: '#5a5757' }}>Copyright © 2021 Video Annotator</div>
+    </footer>
   </Container>
 );
 
