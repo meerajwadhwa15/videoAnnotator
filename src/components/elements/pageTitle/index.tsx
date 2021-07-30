8406a034e162cf466bf4a453ba71164b91ba096b
davenguyenhuy <nguyenhuycuong.uet@gmail.com>
2021-07-30 15:17:09 +0700

    update/refactor video list ui

--- src/components/elements/pageTitle/index.tsx
+++ src/components/elements/pageTitle/index.tsx
@@ -5,13 +5,13 @@ import styles from './style.module.scss';
 
 interface Props {
   title: string;
-  subtitle: string;
+  subtitle?: string;
 }
 
 const PageTitle: FC<Props> = ({ title, subtitle, ...attrs }) => {
   return (
     <Col xs="12" {...attrs} style={{ marginBottom: '20px' }}>
-      <span className={styles.subTitle}>{subtitle}</span>
+      {subtitle && <span className="page-title">{subtitle}</span>}
       <h3 className={styles.title}>{title}</h3>
     </Col>
   );
